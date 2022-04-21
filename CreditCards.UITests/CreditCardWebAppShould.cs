a5d101f1791f3785c7ee70b3ebc457aa4947a82a
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2022-04-21 13:24:44 -0700

    Added tests for loading specific URL, reloading URL, and verifying the correct URL is loaded(no redirects or errors)

--- CreditCards.UITests/CreditCardWebAppShould.cs
+++ CreditCards.UITests/CreditCardWebAppShould.cs
@@ -8,18 +8,40 @@ namespace CreditCards.UITests
 {
     public class CreditCardWebAppShould
     {
+        private const string HomeUrl = "http://localhost:44108/";
+        private const string HomeTitle = "Home Page - Credit Cards";
         [Fact]
-        [Trait("Category","Smoke")]
+        [Trait("Category", "Smoke")]
         public void LoadApplicationPage()
         {
             using (IWebDriver driver = new ChromeDriver())
             {
-                driver.Navigate().GoToUrl("http://localhost:44108/");
+                driver.Navigate().GoToUrl(HomeUrl);
 
-                string pageTitle = driver.Title;
+                DemoHelper.Pause();
 
-                Assert.Equal("Home Page - Credit Cards", pageTitle);
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
             }
+        }
+
+        [Fact]
+        [Trait("Category", "Smoke")]
+        public void ReloadHomePage()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+
+                driver.Navigate().GoToUrl(HomeUrl);
+
+                DemoHelper.Pause();
+
+                driver.Navigate().Refresh();
+
+                Assert.Equal(HomeTitle, driver.Title);
+                Assert.Equal(HomeUrl, driver.Url);
+            }
+            
         }
     }
 }
