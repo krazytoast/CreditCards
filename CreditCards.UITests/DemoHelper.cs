2ddd1dca99354b7a10a7376866457f7c30edd347
Zachery Berkey <31834796+krazytoast@users.noreply.github.com>
2022-04-21 12:59:37 -0700

    Created DemoHelper
    DemoHelper is used for a brief slow down in browser interactions until creating later solutions.

---
+++ CreditCards.UITests/DemoHelper.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+
+namespace CreditCards.UITests
+{
+    internal static class DemoHelper
+    {
+        ///// <summary>
+        ///// Brief delay to slow down browser interactions for
+        ///// demo purposes
+        ///// </summary>
+        
+        public static void Pause(int secondsToPause = 3000)
+        {
+            Thread.Sleep(secondsToPause);
+        }
+    }
+}
